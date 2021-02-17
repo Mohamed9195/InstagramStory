a68ce9b93e36c50700e236eef10ecbc3992dbd40
Mohamed Hashem <mohamedhashem@xtrava.co>
2021-02-17 09:16:57 +0200

    Initial Commit

---
+++ InstagramStory/InstagramStory.h
@@ -0,0 +1,18 @@
+//
+//  InstagramStory.h
+//  InstagramStory
+//
+//  Created by mohamed hashem on 17/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstagramStory.
+FOUNDATION_EXPORT double InstagramStoryVersionNumber;
+
+//! Project version string for InstagramStory.
+FOUNDATION_EXPORT const unsigned char InstagramStoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstagramStory/PublicHeader.h>
+
+
